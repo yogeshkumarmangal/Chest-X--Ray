75edd4f591fcf78c4f3a03abecab905b0f213077
Yogesh Kumar Mangal <42840043+yogeshkumarmangal@users.noreply.github.com>
2021-08-30 13:06:45 +0530

    Update apps.py

--- apps.py
+++ apps.py
@@ -9,7 +9,7 @@ import dash_html_components as html
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+server=app.server
 app.layout = html.Div([
     dcc.Upload(
         id='upload-image',
